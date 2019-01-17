f4d7e84d5f6517ce814bc8139e54c856814adde6
Rémi Le Scaon <remi.lescaon@gmail.com>
2019-01-17 14:22:18 +0000

    fibonacci

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
+from operations import fibo
+
 if __name__ == '__main__':
-    print('Hello World')
+    print('Starting testing..')
+
+    results = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    for i in range(12):
+        assert(fibo(i) == results[i])
